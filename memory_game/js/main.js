50df91b2b38831c47f34f71f0b47b24f31d57c69
James Sinkler <jfullersinkler@gmail.com>
2019-10-09 17:36:20 -0700

    created game logic functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,15 +6,22 @@ var cardFour = "king";
 
 const cards = ['queen', 'king', 'queen', 'king'];
 const cardsInPlay = [];
-cardsInPlay.push(cardOne);
-console.log("User flipped "+ cardsInPlay[0]);
-cardsInPlay.push(cardTwo);
-console.log("User flipped "+ cardsInPlay[1]);
 
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
     if (cardsInPlay[0]===cardsInPlay[1]) {
-        alert("You found a match!");
+        console.log("You found a match!");
     } else {
-        alert("Sorry, try again.");
+        console.log("Sorry, try again.");
     }
 }
+
+function flipCard(cardId) {
+    console.log("User flipped "+ cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+    }
+}
+
+flipCard(0);
+flipCard(1);
